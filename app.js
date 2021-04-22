f2ea6a58b1d69da745f77a5b945371d5f4d52aff
linh <linh.pham@hdwebsoft.co>
2021-04-22 14:01:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer();
+require("dotenv/config");
+
+const app = express();
+const server = require("http").createServer(app);
+
+// middleware
+// app.use(express.static('public'));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+app.use(upload.array());
+
+// import router
+app.use("/posts", require("./routes/posts"));
+
+//router test
+app.get("/", function (req, res) {
+  res.send("<h2>Home</h2>");
+});
+
+// connect db
+const DB_URL = process.env.DB_CONNECTION;
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongo DB"))
+  .catch((err) => console.log("connect mongo DB failed", err));
+
+// start server
+const port = process.env.PORT;
+server.listen(port, () => console.log("server is running on port", port));
---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+
+// get all posts
+router.get("/", async (req, res) => {
+  try {
+    const getPosts = await Post.find();
+    res.json(getPosts);
+  } catch (error) {
+    res.json({ message: err });
+  }
+});
+
+// get posts with postsId
+router.get("/:postsId", async (req, res) => {
+  try {
+    const findPosts = await Post.findById(req.params.postsId);
+    res.json(findPosts);
+  } catch (error) {
+    res.json({ message: err });
+  }
+});
+
+// add posts
+router.post("/", async (req, res) => {
+  const { title, description, date } = req.body;
+  console.log(title, description, date);
+  const post = new Post({ title, description, date });
+  try {
+    const savePosts = await post.save();
+    res.json(savePosts);
+  } catch (error) {
+    res.json({ message: err });
+  }
+});
+
+//delete posts with postsId
+router.delete("/:postsId", async (req, res) => {
+  try {
+    const removePosts = await Post.remove({ _id: req.params.postsId });
+    res.json(removePosts);
+  } catch (error) {
+    res.json({ message: err });
+  }
+});
+
+//update posts with postsId
+router.patch("/:postsId", async (req, res) => {
+  const { title, description, date } = req.body;
+  try {
+    const updatePosts = await Post.updateOne(
+      { _id: req.params.postsId },
+      {
+        $set: { title },
+      }
+    );
+    res.json(updatePosts);
+  } catch (error) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
